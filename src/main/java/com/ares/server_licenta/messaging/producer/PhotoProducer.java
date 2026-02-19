1392739cd9aee83ac55f91539842bbeb1b0cb537
Paul <berindeiepaul8@gmail.com>
2026-02-19 22:24:20 +0200

    TH26-21 Add some loggers and the mapper to json

--- src/main/java/com/ares/server_licenta/messaging/producer/PhotoProducer.java
+++ src/main/java/com/ares/server_licenta/messaging/producer/PhotoProducer.java
@@ -5,11 +5,14 @@ import com.ares.server_licenta.messaging.dto.PhotoMessage;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 public class PhotoProducer {
 
     private final RabbitTemplate rabbitTemplate;
+    private static final Logger LOGGER = LoggerFactory.getLogger(PhotoProducer.class);
 
     public PhotoProducer(RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
@@ -17,6 +20,7 @@ public class PhotoProducer {
 
     public void send(PhotoMessage message) {
 
+        LOGGER.info("Sending message to RabbitMQ");
         rabbitTemplate.convertAndSend(
                 RabbitMQConfig.EXCHANGE,
                 RabbitMQConfig.ROUTING_KEY,
